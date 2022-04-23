e11faa94c232fa97c68b0dfaffac768f669351b2
Dustin Riley <dustin@derpenstiltskin.com>
2022-04-22 21:26:17 -0400

    Cleanup and documentation

--- test/Hudu.spec.js
+++ test/Hudu.spec.js
@@ -1,66 +1,174 @@
 'use strict';
 
-import { expect } from "chai";
-import Hudu from "../src/index.js";
+import { expect } from 'chai';
+import Hudu from '../src/index.js';
 
-describe('Hudu Module', () => {
-    describe('apiConfig object', () => {
-        it('should only accept an object', () => {
-            expect(() => {
-                new Hudu(123);
-            }).to.throw(/API Config must be an object/);
+describe('Hudu', () => {
+    describe('Hudu()', () => {
+        describe('config', () => {
+            it('should only accept an object', () => {
+                expect(() => {
+                    new Hudu(123);
+                }).to.throw(/config must be an object/);
 
-            expect(() => {
-                new Hudu({
-                    host: 'example.com',
-                    key: 'abcdefghijklmnopqrstuvwxyz',
-                });
-            }).to.not.throw();
+                expect(() => {
+                    new Hudu({
+                        host: 'example.com',
+                        key: 'abcdefghijklmnopqrstuvwxyz',
+                    });
+                }).to.not.throw();
+            });
+
+            it('should only contain allowed properties', () => {
+                expect(() => {
+                    new Hudu({
+                        test: 'value',
+                        host: 'example.com',
+                        key: 'abcdefghijklmnopqrstuvwxyz',
+                    });
+                }).to.throw(/config may only contain the following:/);
+
+                expect(() => {
+                    new Hudu({
+                        host: 'example.com',
+                        key: 'abcdefghijklmnopqrstuvwxyz',
+                    });
+                }).to.not.throw();
+            });
+
+            it('should contain host property', () => {
+                expect(() => {
+                    new Hudu({
+                        key: 'abcdefghijklmnopqrstuvwxyz',
+                    });
+                }).to.throw(/config must contain host value/);
+
+                expect(() => {
+                    new Hudu({
+                        host: 'example.com',
+                        key: 'abcdefghijklmnopqrstuvwxyz',
+                    });
+                }).to.not.throw();
+            });
+
+            it('should contain key property', () => {
+                expect(() => {
+                    new Hudu({
+                        host: 'example.com',
+                    });
+                }).to.throw(/config must contain key value/);
+
+                expect(() => {
+                    new Hudu({
+                        host: 'example.com',
+                        key: 'abcdefghijklmnopqrstuvwxyz',
+                    });
+                }).to.not.throw();
+            });
         });
+    });
 
-        it('should only contain allowed properties', () => {
-            expect(() => {
-                new Hudu({
-                    test: 'value',
-                });
-            }).to.throw();
+    describe('_sendApiRequest()', () => {
+        describe('options', () => {
+            it('should only accept an object', () => {
+                expect(() => {
+                    const huduInst = new Hudu({
+                        host: 'example.com',
+                        key: 'abcdefghijklmnopqrstuvwxyz',
+                    });
 
-            expect(() => {
-                new Hudu({
-                    host: 'example.com',
-                    key: 'abcdefghijklmnopqrstuvwxyz',
-                });
-            }).to.not.throw();
-        });
+                    huduInst._sendApiRequest(123);
+                }).to.throw(/options must be an object/);
 
-        it('should contain host property', () => {
-            expect(() => {
-                new Hudu({
-                    key: 'abcdefghijklmnopqrstuvwxyz',
-                });
-            }).to.throw(/API Config must contain host value/);
+                expect(() => {
+                    const huduInst = new Hudu({
+                        host: 'example.com',
+                        key: 'abcdefghijklmnopqrstuvwxyz',
+                    });
 
-            expect(() => {
-                new Hudu({
-                    host: 'example.com',
-                    key: 'abcdefghijklmnopqrstuvwxyz',
-                });
-            }).to.not.throw();
-        });
+                    huduInst._sendApiRequest({
+                        method: 'get',
+                        resource: 'companies',
+                    });
+                }).to.not.throw();
+            });
 
-        it('should contain key property', () => {
-            expect(() => {
-                new Hudu({
-                    host: 'example.com',
-                });
-            }).to.throw(/API Config must contain key value/);
+            it('should only contain allowed properties', () => {
+                expect(() => {
+                    const huduInst = new Hudu({
+                        host: 'example.com',
+                        key: 'abcdefghijklmnopqrstuvwxyz',
+                    });
 
-            expect(() => {
-                new Hudu({
-                    host: 'example.com',
-                    key: 'abcdefghijklmnopqrstuvwxyz',
-                });
-            }).to.not.throw();
+                    huduInst._sendApiRequest({
+                        test: 'value',
+                        method: 'get',
+                        resource: 'companies',
+                    });
+                }).to.throw(/options may only contain the following:/);
+
+                expect(() => {
+                    const huduInst = new Hudu({
+                        host: 'example.com',
+                        key: 'abcdefghijklmnopqrstuvwxyz',
+                    });
+
+                    huduInst._sendApiRequest({
+                        method: 'get',
+                        resource: 'companies',
+                    });
+                }).to.not.throw();
+            });
+
+            it('should contain method property', () => {
+                expect(() => {
+                    const huduInst = new Hudu({
+                        host: 'example.com',
+                        key: 'abcdefghijklmnopqrstuvwxyz',
+                    });
+
+                    huduInst._sendApiRequest({
+                        resource: 'companies',
+                    });
+                }).to.throw(/options must contain method value/);
+
+                expect(() => {
+                    const huduInst = new Hudu({
+                        host: 'example.com',
+                        key: 'abcdefghijklmnopqrstuvwxyz',
+                    });
+
+                    huduInst._sendApiRequest({
+                        method: 'get',
+                        resource: 'companies',
+                    });
+                }).to.not.throw();
+            });
+
+            it('should contain resource property', () => {
+                expect(() => {
+                    const huduInst = new Hudu({
+                        host: 'example.com',
+                        key: 'abcdefghijklmnopqrstuvwxyz',
+                    });
+
+                    huduInst._sendApiRequest({
+                        method: 'get',
+                    });
+                }).to.throw(/options must contain resource value/);
+
+                expect(() => {
+                    const huduInst = new Hudu({
+                        host: 'example.com',
+                        key: 'abcdefghijklmnopqrstuvwxyz',
+                    });
+
+                    huduInst._sendApiRequest({
+                        method: 'get',
+                        resource: 'companies',
+                    });
+                }).to.not.throw();
+            });
         });
     });
 });
